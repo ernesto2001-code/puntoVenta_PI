a1ff1e39241e173a3b57c7378cc00278026d67e2
ernesto2001-code <ejimenez17@ucol.mx>
2021-12-05 17:54:11 -0600

    Update config.php

--- src/config.php
+++ src/config.php
@@ -36,30 +36,30 @@ if ($_POST) {
 <div class="col-md-6 mx-auto">
             <div class="card">
                 <div class="card-header bg-primary text-white">
-                    Datos de la Empresa
+                    Datos Empresa
                 </div>
                 <div class="card-body">
                     <form action="" method="post" class="p-3">
                         <div class="form-group">
                             <label>Nombre:</label>
                             <input type="hidden" name="id" value="<?php echo $data['id'] ?>">
-                            <input type="text" name="nombre" class="form-control" value="<?php echo $data['nombre']; ?>" id="txtNombre" placeholder="Nombre de la Empresa" required class="form-control">
+                            <input type="text" name="nombre" class="form-control" value="<?php echo $data['nombre']; ?>" id="txtNombre" placeholder="Nombre empresa" required class="form-control">
                         </div>
                         <div class="form-group">
                             <label>Teléfono:</label>
-                            <input type="number" name="telefono" class="form-control" value="<?php echo $data['telefono']; ?>" id="txtTelEmpresa" placeholder="teléfono de la Empresa" required>
+                            <input type="number" name="telefono" class="form-control" value="<?php echo $data['telefono']; ?>" id="txtTelEmpresa" placeholder="teléfono empresa" required>
                         </div>
                         <div class="form-group">
                             <label>Correo Electrónico:</label>
-                            <input type="email" name="email" class="form-control" value="<?php echo $data['email']; ?>" id="txtEmailEmpresa" placeholder="Correo de la Empresa" required>
+                            <input type="email" name="email" class="form-control" value="<?php echo $data['email']; ?>" id="txtEmailEmpresa" placeholder="Correo empresa" required>
                         </div>
                         <div class="form-group">
                             <label>Dirección:</label>
-                            <input type="text" name="direccion" class="form-control" value="<?php echo $data['direccion']; ?>" id="txtDirEmpresa" placeholder="Dirreción de la Empresa" required>
+                            <input type="text" name="direccion" class="form-control" value="<?php echo $data['direccion']; ?>" id="txtDirEmpresa" placeholder="Dirreción empresa" required>
                         </div>
                         <?php echo isset($alert) ? $alert : ''; ?>
                         <div>
-                            <button type="submit" class="btn btn-primary"><i class="fas fa-save"></i> Modificar Datos</button>
+                            <button type="submit" class="btn btn-primary"><i class="fas fa-save"></i> Guardar</button>
                         </div>
 
                     </form>
