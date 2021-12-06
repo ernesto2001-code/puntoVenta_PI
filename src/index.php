45773552fcb6424690d6dd7bb4727ada8ff75da2
WilliamRVazquez <wvazquez0@ucol.mx>
2021-12-05 21:30:53 -0600

    ventas actualizado

--- src/index.php
+++ src/index.php
@@ -48,7 +48,7 @@ $totalV = mysqli_num_rows($ventas);
             </div>
         </a>
 
-        <a class="col-xl-3 col-md-6 mb-4" href="ventas.php">
+        <a class="col-xl-3 col-md-6 mb-4" href="lista_ventas.php">
             <div class="card border-left-warning bg-danger shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
