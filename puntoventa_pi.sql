6ee6eb09bc509e34803b148aa47aa0166709d121
ernesto2001-code <ejimenez17@ucol.mx>
2021-12-06 01:21:16 -0600

    Create puntoventa_pi.sql

---
+++ puntoventa_pi.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-12-2021 a las 05:42:30
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `puntoventa_pi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_permisos`
+--
+
+CREATE TABLE `detalle_permisos` (
+  `id` int(11) NOT NULL,
+  `id_permiso` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `detalle_permisos`
+--
+
+INSERT INTO `detalle_permisos` (`id`, `id_permiso`, `id_usuario`) VALUES
+(8, 3, 2),
+(9, 4, 2),
+(10, 5, 2),
+(11, 6, 2),
+(14, 4, 4),
+(15, 6, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_temp`
+--
+
+CREATE TABLE `detalle_temp` (
+  `id` int(11) NOT NULL,
+  `id_usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta` decimal(10,2) NOT NULL,
+  `total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ventas`
+--
+
+CREATE TABLE `detalle_ventas` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `total` decimal(9,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `detalle_ventas`
+--
+
+INSERT INTO `detalle_ventas` (`id`, `usuario`, `fecha`, `total`) VALUES
+(1, 'william ramses', '2021-12-06', '61.20'),
+(3, 'ernesto manuel', '2021-12-06', '172.50'),
+(4, 'william ramses', '2021-12-06', '34.50'),
+(5, 'william ramses', '2021-12-06', '491.50');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`id`, `nombre`) VALUES
+(2, 'usuarios'),
+(4, 'productos'),
+(5, 'ventas'),
+(6, 'nueva_venta');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `codproducto` int(11) NOT NULL,
+  `codigo` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `existencia` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`codproducto`, `codigo`, `descripcion`, `precio`, `existencia`, `usuario_id`, `estado`) VALUES
+(7, '750120970102', 'Leche Sello Rojo	1L', '20.40', 28, 1, 1),
+(8, '750120970111', 'Leche Sello Rojo 1.8L', '34.00', 30, 1, 1),
+(9, '750105532834', 'Coca-Cola 400ml', '7.00', 30, 1, 1),
+(10, '750105530524', 'Coca-Cola 2.5L', '36.00', 29, 1, 1),
+(11, '750103049107', 'Príncipe Marinela 147g', '13.25', 25, 1, 1),
+(12, '750047801364', 'Galleta Emperador 91g', '16.50', 25, 1, 1),
+(13, '75752804052', 'Papas Barcel Chips Fuego 240g', '51.00', 25, 1, 1),
+(14, '75752800186', 'Papas Barcel Chips Fuego  55g', '18.00', 20, 1, 1),
+(15, '4133300102', 'Duracell Copper And Black Pilas Alcalinas AA 6 Piezas', '149.00', 20, 1, 1),
+(16, '750108680063', 'Agua Natural Epura  2L', '15.00', 30, 1, 1),
+(17, '750108680104', 'Agua Natural Epura  1L', '7.30', 30, 1, 1),
+(18, '65024001053', 'Tabletas Next Antigripal	10 tabletas', '29.90', 15, 1, 1),
+(19, '7033071756', 'Rastrillo Desechable Bic 1', '15.00', 10, 1, 1),
+(20, '61300873876', 'Te Arizona Sandia	680ml', '12.90', 25, 1, 1),
+(21, '61300873884', 'Té Arizona Limón  680ml', '12.90', 25, 1, 1),
+(22, '750630624479', 'Desodorante Aerosol Axe  150ml', '47.90', 15, 1, 1),
+(23, '750104000022', 'Jamon de Pavo Fud  170g', '31.50', 13, 1, 1),
+(24, '750104000975', 'Salchichas Fud   266g', '27.50', 13, 1, 1),
+(25, '762221056153', 'Queso Philadelphia	200g', '34.50', 15, 1, 1),
+(26, '750104008033', 'Queso La Villita   400g', '63.50', 15, 1, 1),
+(27, '770201063119', 'Cepillo de Dientes Colgate 1 ', '10.00', 20, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `correo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `clave` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `nombre`, `correo`, `usuario`, `clave`, `estado`) VALUES
+(1, 'william ramses', 'william@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
+(4, 'ernesto manuel', 'ernesto@gmail.com', 'ernesto', '3411f6d521ed0d17b6953e5741eaecca', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `detalle_permisos`
+--
+ALTER TABLE `detalle_permisos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`codproducto`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_permisos`
+--
+ALTER TABLE `detalle_permisos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
