991e3a191cd64fafe02a5e121a88ef0715cb50fb
WilliamRVazquez <wvazquez0@ucol.mx>
2021-12-10 09:40:52 -0600

    Cambios

--- puntoventa_pi.sql
+++ puntoventa_pi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-12-2021 a las 03:46:39
--- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 8.0.1
+-- Tiempo de generación: 10-12-2021 a las 16:23:25
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,50 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `puntoventa_pi`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `idcliente` int(11) NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `telefono` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
-  `direccion` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `estado` int(11) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `cliente`
---
-
-INSERT INTO `cliente` (`idcliente`, `nombre`, `telefono`, `direccion`, `usuario_id`, `estado`) VALUES
-(1, 'Público en General', '8296826298', 'S/D', 1, 1),
-(2, 'Angel sifuentes flores', '2147483647', 'Lima - Huari', 1, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `configuracion`
---
-
-CREATE TABLE `configuracion` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `telefono` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `direccion` text COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `configuracion`
---
-
-INSERT INTO `configuracion` (`id`, `nombre`, `telefono`, `email`, `direccion`) VALUES
-(1, 'Abarrotes Don Chuy', '523141234567', 'wvazquez0@ucol.mx', 'Valle del Coral');
 
 -- --------------------------------------------------------
 
@@ -90,20 +46,6 @@ INSERT INTO `detalle_permisos` (`id`, `id_permiso`, `id_usuario`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `detalle_ventas`
---
-
-CREATE TABLE `detalle_ventas` (
-  `idDetalleVenta` int(11) NOT NULL,
-  `ventaIdVenta` int(11) NOT NULL,
-  `producto_IdProductos` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `precioVenta` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `permisos`
 --
 
@@ -121,49 +63,6 @@ INSERT INTO `permisos` (`id`, `nombre`) VALUES
 (4, 'productos'),
 (5, 'ventas'),
 (6, 'nueva_venta');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto`
---
-
-CREATE TABLE `producto` (
-  `codproducto` int(11) NOT NULL,
-  `codigo` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
-  `descripcion` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `existencia` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `estado` int(11) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `producto`
---
-
-INSERT INTO `producto` (`codproducto`, `codigo`, `descripcion`, `precio`, `existencia`, `usuario_id`, `estado`) VALUES
-(7, '750105534960', 'Coca Cola 255ml', '6.00', 25, 1, 1),
-(8, '750120970102', 'Leche Sello Rojo 1Lt', '31.00', 25, 1, 1),
-(9, '750105532834', 'Coca-Cola 400ml', '7.00', 30, 1, 1),
-(10, '750105530524', 'Coca-Cola 2.5L', '36.00', 30, 1, 1),
-(11, '750103049107', 'Príncipe Marinela 147g', '13.25', 25, 1, 1),
-(12, '750047801364', 'Galleta Emperador 91g', '16.50', 25, 1, 1),
-(13, '75752804052', 'Papas Barcel Chips Fuego 240g', '51.00', 25, 1, 1),
-(14, '75752800186', 'Papas Barcel Chips Fuego  55g', '18.00', 20, 1, 1),
-(15, '4133300102', 'Duracell Copper And Black Pilas Alcalinas AA 6 Piezas', '149.00', 20, 1, 1),
-(16, '750108680063', 'Agua Natural Epura  2L', '15.00', 30, 1, 1),
-(17, '750108680104', 'Agua Natural Epura  1L', '7.30', 30, 1, 1),
-(18, '65024001053', 'Tabletas Next Antigripal	10 tabletas', '29.90', 15, 1, 1),
-(19, '7033071756', 'Rastrillo Desechable Bic 1', '15.00', 10, 1, 1),
-(20, '61300873876', 'Te Arizona Sandia	680ml', '12.90', 25, 1, 1),
-(21, '61300873884', 'Té Arizona Limón  680ml', '12.90', 25, 1, 1),
-(22, '750630624479', 'Desodorante Aerosol Axe  150ml', '47.90', 15, 1, 1),
-(23, '750104000022', 'Jamon de Pavo Fud  170g', '31.50', 13, 1, 1),
-(24, '750104000975', 'Salchichas Fud   266g', '27.50', 13, 1, 1),
-(25, '762221056153', 'Queso Philadelphia	200g', '34.50', 15, 1, 1),
-(26, '750104008033', 'Queso La Villita   400g', '63.50', 15, 1, 1),
-(27, '770201063119', 'Cepillo de Dientes Colgate 1 ', '10.00', 20, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -186,7 +85,7 @@ CREATE TABLE `productos` (
 
 INSERT INTO `productos` (`id`, `codigo`, `descripcion`, `precioVenta`, `precioCompra`, `existencia`) VALUES
 (6, '750120970102', 'Leche Sello Rojo	1L', '20.40', '20.00', '92.00'),
-(7, '750120970111', 'Leche Sello Rojo 1.8L', '34.00', '30.00', '100.00'),
+(7, '750120970111', 'Leche Sello Rojo 1.8L', '34.00', '30.00', '99.00'),
 (8, '750105532834', 'Coca-Cola 400ml', '7.00', '5.00', '100.00'),
 (9, '750105530524', 'Coca-Cola 2.5L', '36.00', '30.00', '100.00'),
 (10, '750103049107', 'Príncipe Marinela 147g', '13.25', '10.00', '100.00'),
@@ -196,16 +95,16 @@ INSERT INTO `productos` (`id`, `codigo`, `descripcion`, `precioVenta`, `precioCo
 (14, '4133300102', 'Duracell Copper And Black Pilas Alcalinas AA 6 Piezas', '149.00', '140.00', '100.00'),
 (15, '750108680063', 'Agua Natural Epura  2L', '15.00', '10.00', '100.00'),
 (16, '750108680104', 'Agua Natural Epura  1L', '7.30', '6.00', '100.00'),
-(17, '65024001053', 'Tabletas Next Antigripal	10 tabletas', '29.90', '25.00', '100.00'),
+(17, '65024001053', 'Tabletas Next Antigripal	10 tabletas', '29.90', '25.00', '99.00'),
 (18, '7033071756', 'Rastrillo Desechable Bic 1', '15.00', '10.00', '100.00'),
-(19, '61300873876', 'Te Arizona Sandia	680ml', '12.90', '10.00', '100.00'),
+(19, '61300873876', 'Te Arizona Sandia	680ml', '12.90', '10.00', '99.00'),
 (20, '61300873884', 'Té Arizona Limón  680ml', '12.90', '10.00', '100.00'),
 (21, '750630624479', 'Desodorante Aerosol Axe  150ml', '47.90', '40.00', '100.00'),
 (22, '750104000022', 'Jamon de Pavo Fud  170g', '31.50', '30.00', '100.00'),
 (23, '750104000975', 'Salchichas Fud   266g', '27.50', '25.00', '100.00'),
 (24, '762221056153', 'Queso Philadelphia	200g', '34.50', '30.00', '100.00'),
 (25, '750104008033', 'Queso La Villita   400g', '63.50', '60.00', '100.00'),
-(26, '770201063119', 'Cepillo de Dientes Colgate 1 ', '10.00', '7.00', '100.00');
+(26, '770201063119', 'Cepillo de Dientes Colgate 1 ', '10.00', '7.00', '99.00');
 
 -- --------------------------------------------------------
 
@@ -219,6 +118,16 @@ CREATE TABLE `productos_vendidos` (
   `cantidad` bigint(20) UNSIGNED NOT NULL,
   `id_venta` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `productos_vendidos`
+--
+
+INSERT INTO `productos_vendidos` (`id`, `id_producto`, `cantidad`, `id_venta`) VALUES
+(1, 17, 1, 7),
+(2, 7, 1, 9),
+(3, 19, 1, 9),
+(4, 26, 1, 9);
 
 -- --------------------------------------------------------
 
@@ -246,55 +155,27 @@ INSERT INTO `usuario` (`idusuario`, `nombre`, `correo`, `usuario`, `clave`, `est
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `venta`
+-- Estructura de tabla para la tabla `ventas`
 --
 
-CREATE TABLE `venta` (
+CREATE TABLE `ventas` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `fecha` datetime NOT NULL,
   `total` decimal(7,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `venta`
+-- Volcado de datos para la tabla `ventas`
 --
 
-INSERT INTO `venta` (`id`, `fecha`, `total`) VALUES
-(1, '2021-12-10 03:13:06', '31.00'),
-(2, '2021-12-10 03:15:42', '62.00'),
-(3, '2021-12-10 03:30:25', '62.00'),
-(4, '2021-12-10 03:33:25', '20.40'),
-(5, '2021-12-10 03:33:55', '61.20'),
-(6, '2021-12-10 03:34:15', '81.60');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ventas`
---
-
-CREATE TABLE `ventas` (
-  `idVenta` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `fecha` date NOT NULL DEFAULT current_timestamp(),
-  `total` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `ventas` (`id`, `fecha`, `total`) VALUES
+(7, '2021-12-10 04:42:27', '29.90'),
+(8, '2021-12-10 04:47:51', '0.00'),
+(9, '2021-12-10 04:48:24', '56.90');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`idcliente`);
-
---
--- Indices de la tabla `configuracion`
---
-ALTER TABLE `configuracion`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `detalle_permisos`
@@ -303,25 +184,10 @@ ALTER TABLE `detalle_permisos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `detalle_ventas`
---
-ALTER TABLE `detalle_ventas`
-  ADD PRIMARY KEY (`idDetalleVenta`),
-  ADD KEY `ventaIdVenta` (`ventaIdVenta`),
-  ADD KEY `producto_IdProductos` (`producto_IdProductos`);
-
---
 -- Indices de la tabla `permisos`
 --
 ALTER TABLE `permisos`
   ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`codproducto`),
-  ADD KEY `productos_vendidos_ibfk_1` (`codproducto`) USING BTREE;
 
 --
 -- Indices de la tabla `productos`
@@ -344,33 +210,14 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idusuario`);
 
 --
--- Indices de la tabla `venta`
---
-ALTER TABLE `venta`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`idVenta`),
-  ADD KEY `fk_usuario_idUsuario` (`id_usuario`) USING BTREE;
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `configuracion`
---
-ALTER TABLE `configuracion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_permisos`
@@ -379,22 +226,10 @@ ALTER TABLE `detalle_permisos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT de la tabla `detalle_ventas`
---
-ALTER TABLE `detalle_ventas`
-  MODIFY `idDetalleVenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT de la tabla `permisos`
 --
 ALTER TABLE `permisos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `producto`
---
-ALTER TABLE `producto`
-  MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -406,7 +241,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `productos_vendidos`
 --
 ALTER TABLE `productos_vendidos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -415,40 +250,21 @@ ALTER TABLE `usuario`
   MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `venta`
---
-ALTER TABLE `venta`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `idVenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `detalle_ventas`
---
-ALTER TABLE `detalle_ventas`
-  ADD CONSTRAINT `detalle_ventas_ibfk_1` FOREIGN KEY (`ventaIdVenta`) REFERENCES `ventas` (`idVenta`) ON DELETE CASCADE,
-  ADD CONSTRAINT `detalle_ventas_ibfk_2` FOREIGN KEY (`producto_IdProductos`) REFERENCES `producto` (`codproducto`) ON DELETE CASCADE;
-
---
 -- Filtros para la tabla `productos_vendidos`
 --
 ALTER TABLE `productos_vendidos`
   ADD CONSTRAINT `productos_vendidos_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `productos_vendidos_ibfk_2` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id`) ON DELETE CASCADE;
-
---
--- Filtros para la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE;
+  ADD CONSTRAINT `productos_vendidos_ibfk_2` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
